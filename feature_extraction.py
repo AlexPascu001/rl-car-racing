3b174759eba63d4b198e9188775f15fdfff0f3ee
Stan Catalin <48752183+cata1212112@users.noreply.github.com>
2023-12-17 18:10:53 +0200

    Improve extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -29,7 +29,7 @@ def extract_true_speed(image):
 
     # Extract the true speed bar.
     speed_bar = image[down_y:up_y, left_x:right_x]
-    
+
     # Sum for each line in the speed bar.
     speed_bar = np.flip(np.sum(speed_bar, axis=(1,2)))
 
@@ -44,8 +44,8 @@ def extract_steering(image):
     s = IMAGE_WIDTH / 40.0
     h = IMAGE_HEIGHT / 40.0
 
-    left_x = math.floor((place - 4.5) * s)
-    right_x = math.floor((place + 4.5) * s)
+    left_x = math.floor((place - 4.2) * s)
+    right_x = math.floor((place + 4.2) * s)
     center_x = math.floor(place * s)
     line_y = math.floor((2 * IMAGE_HEIGHT - 5 * h) / 2) - 1
 
@@ -54,12 +54,16 @@ def extract_steering(image):
     # image = cv.line(image, (center_x, -1000), (center_x, 1000), (0, 0, 255), 1)
     # image = cv.line(image, (-1000, line_y), (1000, line_y), (0, 0, 255), 1)
     # cv.imwrite(f"auxiliaryImages/{extract_steering.cnt}.jpg", image)
+
+    green_channel = image[:, :, 1]
+    green_channel[green_channel > 0] = 255
+
     extract_steering.cnt += 1
 
-    steering_val_left = np.sum(image[line_y, left_x - 1:center_x, 1])
-    steering_val_right = np.sum(image[line_y, center_x:right_x + 1, 1])
+    steering_val_left = np.sum(image[line_y, left_x - 1:center_x + 1, 1])
+    steering_val_right = np.sum(image[line_y, center_x - 1:right_x + 1, 1])
 
-    steering_val_maximum = max(center_x - left_x, right_x - center_x) * 255
+    steering_val_maximum = max(center_x - left_x + 1, right_x - center_x + 1) * 255
 
     steering_val_left_normalized = steering_val_left * 0.42 / steering_val_maximum
     steering_val_right_normalized = steering_val_right * 0.42 / steering_val_maximum
