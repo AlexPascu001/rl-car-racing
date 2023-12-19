fff13c211ba935d3a4ab10974db3e3a7c9440c35
AlexPascu001 <ionut-alexandru.pascu@s.unibuc.ro>
2023-12-19 22:18:30 +0200

    extract gyroscope sensor and show delta between true and detected data

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import gymnasium as gym
 import pygame
 import numpy as np
 import feature_extraction
+import cv2 as cv
 
 MAX_STEPS = 100000000
-SCREEN_WIDTH = 400 
-SCREEN_HEIGHT = 400 
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
 # User-controlled input.
 a = np.array([0.0, 0.0, 0.0])
+
+MAX_GYROSCOPE_VAL = 22  # found by trial and error
 
 def register_input():
     global a
@@ -22,6 +25,10 @@ def register_input():
                 a[1] = +1.0
             if event.key == pygame.K_DOWN:
                 a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
+            # to close the game
+            if event.key == pygame.K_ESCAPE:
+                env.close()
+                exit()
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -33,12 +40,14 @@ def register_input():
             if event.key == pygame.K_DOWN:
                 a[2] = 0
 
+
 # Create the pygame screen, used for debugging.
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Create the environment.
 env = gym.make("CarRacing-v2", max_episode_steps=MAX_STEPS)
 obs, info = env.reset()
+
 
 while True:
     register_input()
@@ -47,28 +56,41 @@ while True:
     # Render the observation using pygame.
     # Rotate + flip the observation, see: 
     # https://stackoverflow.com/questions/66241275/pygame-rotates-camera-stream-from-opencv-camera.
-    rotated = np.rot90(observation, k=1, axes=(0,1))
+    rotated = np.rot90(observation, k=1, axes=(0, 1))
     flipped = np.flip(rotated, axis=0)
     extracted_speed = feature_extraction.extract_true_speed(observation)
     extracted_abs = feature_extraction.extract_abs(observation)
+    indicator_bar = feature_extraction.extract_indicators(flipped)
 
     true_speed = np.sqrt(
-            np.square(env.car.hull.linearVelocity[0])
-            + np.square(env.car.hull.linearVelocity[1])
-        )
+        np.square(env.car.hull.linearVelocity[0])
+        + np.square(env.car.hull.linearVelocity[1])
+    )
     true_abs = tuple(env.car.wheels[i].omega for i in range(4))
-    print(f"Extracted speed: {extracted_speed}, Actual: {true_speed}")
-    print(f"Extracted abs: {extracted_abs}, Actual: {true_abs}")
 
-    surface = pygame.surfarray.make_surface(flipped)
+
+    indicator_h, indicator_w, ch = indicator_bar.shape
+
+    indicator_bar_resized = cv.resize(indicator_bar, (indicator_w * 8, indicator_h), interpolation=cv.INTER_AREA)
+    current_gyroscope_value = feature_extraction.extract_gyroscope(indicator_bar_resized)
+    current_gyroscope_value = current_gyroscope_value / MAX_GYROSCOPE_VAL
+
+    # print(f"True speed: {true_speed:.2f} | Extracted speed: {extracted_speed:.2f}")
+    # print(f"True abs: {true_abs} | Extracted abs: {extracted_abs}")
+    # print(f"Current gyroscope value: {current_gyroscope_value:.2f}")
+
+    print(f"Delta true speed: {true_speed - extracted_speed:.2f} | Delta true abs: {true_abs[0] - extracted_abs[0]:.2f} | Gyroscope: {current_gyroscope_value:.2f}")
+
+    surface = pygame.surfarray.make_surface(indicator_bar)
 
     # Scale the surface to the screen size.
     surface = pygame.transform.scale(surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    screen.blit(surface, (0,0))
+    screen.blit(surface, (0, 0))
     pygame.display.update()
 
     if terminated or truncated:
         break
 
+
 env.close()
+
